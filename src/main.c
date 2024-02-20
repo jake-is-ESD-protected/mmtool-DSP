e4a6efd42e12027a7293e26a0be5b0159159cd39
jake-is-ESD-protected <jt@tschavoll.at>
2024-02-20 23:22:13 +0100

    blinky test abstraction

--- src/main.c
+++ src/main.c
@@ -1,10 +1,12 @@
+#include "port.h"
 
-    #include "port.h"
+void port_setup(void){
 
-    void port_setup(void){
-  
-    }
+}
 
-    void port_loop(void){
-
-    }
+void port_loop(void){
+    HAL_GPIO_WritePin(LED_PIN_GPIO_Port, LED_PIN_Pin, GPIO_PIN_SET);
+    HAL_Delay(500);
+    HAL_GPIO_WritePin(LED_PIN_GPIO_Port, LED_PIN_Pin, GPIO_PIN_RESET);
+    HAL_Delay(500);
+}
