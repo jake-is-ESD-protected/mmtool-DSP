5ccc30d1e96816b723e6fd0120159c73c6ade3fb
jake-is-ESD-protected <jt@tschavoll.at>
2024-02-23 16:01:42 +0100

    process SPI data in main loop via flag

--- src/main.c
+++ src/main.c
@@ -6,14 +6,18 @@
 uint8_t rx_buf[BUF_SIZE] = {0};
 uint8_t tx_buf[BUF_SIZE] = {0};
 uint8_t large_buf[128] = {0};
-uint16_t i = 0;
+uint8_t i = 0;
+uint8_t restart = 0;
 
 void port_setup(void){
     HAL_SPI_Receive_DMA(&hspi1, rx_buf, BUF_SIZE);
 }
 
 void port_loop(void){
-    HAL_SPI_Receive_DMA(&hspi1, rx_buf, BUF_SIZE);
+    if(restart){
+        HAL_SPI_Receive_DMA(&hspi1, rx_buf, BUF_SIZE);
+        restart = 0;
+    }
     // HAL_GPIO_WritePin(LED_PIN_GPIO_Port, LED_PIN_Pin, GPIO_PIN_SET);
     // HAL_Delay(500);
     // HAL_GPIO_WritePin(LED_PIN_GPIO_Port, LED_PIN_Pin, GPIO_PIN_RESET);
@@ -21,15 +25,17 @@ void port_loop(void){
 }
 
 void HAL_SPI_RxHalfCpltCallback(SPI_HandleTypeDef *hspi){
-
+    
+    
 }
 
 void HAL_SPI_RxCpltCallback(SPI_HandleTypeDef *hspi){
-    memcpy(tx_buf, rx_buf, BUF_SIZE);
-    HAL_SPI_Transmit_DMA(&hspi1, tx_buf, BUF_SIZE);
-    large_buf[i++] = rx_buf[0];
-    if(i == 128){
+    uint8_t val = rx_buf[0]++;
+    val++;
+    large_buf[i++] = val;
+    HAL_SPI_Transmit_DMA(&hspi1, &val, BUF_SIZE);
+    if(i == 32){
         i = 0;
     }
+    restart = 1;
 }
-
