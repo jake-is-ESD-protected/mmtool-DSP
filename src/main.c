dacc745855ca15b46a3b7e65f3da33f298785655
jake-is-ESD-protected <jt@tschavoll.at>
2024-02-25 00:41:18 +0100

    move spi stuff to module, clean main

--- src/main.c
+++ src/main.c
@@ -1,41 +1,25 @@
+/**
+ * @file main.c
+ * @author jake-is-ESD-protected
+ * @date 2024-02-25
+ * @brief Main program entry. Configured in Arduino-style.
+ *
+ * Any calls by the HAL itself are done before `port_setup()`
+ * is called. This function and this file in general can be
+ * treated as user-level code, which sits above the HAL code.
+ */
+
+
 #include "port.h"
 #include "memory.h"
-
-#define BUF_SIZE 1
-
-uint8_t rx_buf[BUF_SIZE] = {0};
-uint8_t tx_buf[BUF_SIZE] = {0};
-uint8_t large_buf[128] = {0};
-uint8_t i = 0;
-uint8_t restart = 1;
+#include "spi.h"
 
 void port_setup(void){
-
+    spi_init();
 }
+
 
 void port_loop(void){
-    if(restart){
-        HAL_SPI_Receive_DMA(&hspi1, rx_buf, BUF_SIZE);
-        restart = 0;
-    }
-    // HAL_GPIO_WritePin(LED_PIN_GPIO_Port, LED_PIN_Pin, GPIO_PIN_SET);
-    // HAL_Delay(500);
-    // HAL_GPIO_WritePin(LED_PIN_GPIO_Port, LED_PIN_Pin, GPIO_PIN_RESET);
-    // HAL_Delay(500);
+
 }
 
-void HAL_SPI_RxHalfCpltCallback(SPI_HandleTypeDef *hspi){
-    
-    
-}
-
-void HAL_SPI_RxCpltCallback(SPI_HandleTypeDef *hspi){
-    uint8_t val = rx_buf[0]++;
-    val++;
-    large_buf[i++] = val;
-    HAL_SPI_Transmit_DMA(&hspi1, &val, BUF_SIZE);
-    if(i == 32){
-        i = 0;
-    }
-    restart = 1;
-}
