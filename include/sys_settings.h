a01377aac723f95966a07d1109e0b341203cbf46
jake-is-ESD-protected <jt@tschavoll.at>
2024-02-25 00:40:28 +0100

    add system setting macros for compile time changes

---
+++ include/sys_settings.h
@@ -0,0 +1,16 @@
+/**
+ * @file sys_settings.h
+ * @author jake-is-ESD-protected
+ * @date 2024-02-24
+ * @brief Settings for changeable parameters during compile time.
+ *
+ * 
+ */
+
+#ifndef _SYS_SETTINGS_H_
+#define _SYS_SETTINGS_H_
+
+#define SYS_USE_LED_LOOP_ERR_HANDLER
+#define SYS_LED_LOOP_ERR_BLINK_TIME_MS  200
+
+#endif // _SYS_SETTINGS_H_
