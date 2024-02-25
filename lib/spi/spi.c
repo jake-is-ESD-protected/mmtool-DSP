00864552d040a7fc3ed5689f627d9211e3e36ee5
jake-is-ESD-protected <jt@tschavoll.at>
2024-02-26 00:44:25 +0100

    robust SPI command and data block which cost me hours and 50mg of nerve cells

--- lib/spi/spi.c
+++ lib/spi/spi.c
@@ -8,19 +8,26 @@
  */
 
 #include "spi.h"
+#include "cross_platform.h"
 
-SPI_HandleTypeDef* spi_p = NULL;
+static SPI_HandleTypeDef* spi_p = NULL;
 
-uint8_t tx_buf[SPI_TX_BUF_SIZE] = {0};
-uint8_t rx_buf[SPI_RX_BUF_SIZE] = {0};
+static uint8_t tx_buf[SPI_TX_BUF_SIZE] = {0};
+static uint8_t rx_buf[SPI_RX_BUF_SIZE] = {0};
+
+static spi_instruction_t instr = {0};
 
 void spi_init(void){
     spi_p = &hspi1;
+    spi_cmd_handshake();
 }
 
+void spi_deinit(void){
+    spi_p = NULL;
+}
 
-HAL_StatusTypeDef spi_tx_internal(void){
-    return HAL_SPI_Transmit_DMA(spi_p, tx_buf, SPI_TX_BUF_SIZE);
+HAL_StatusTypeDef spi_tx_internal(uint16_t size){
+    return HAL_SPI_Transmit_DMA(spi_p, tx_buf, size);
 }
 
 
@@ -29,8 +36,8 @@ HAL_StatusTypeDef spi_tx_external(uint8_t *pData, uint16_t size){
 }
 
 
-HAL_StatusTypeDef spi_rx_internal(void){
-    return HAL_SPI_Receive_DMA(spi_p, rx_buf, SPI_RX_BUF_SIZE);
+HAL_StatusTypeDef spi_rx_internal(uint16_t size){
+    return HAL_SPI_Receive_DMA(spi_p, rx_buf, size);
 }
 
 
@@ -39,30 +46,33 @@ HAL_StatusTypeDef spi_rx_external(uint8_t *pData, uint16_t size){
 }
 
 
-void HAL_SPI_TxHalfCpltCallback(SPI_HandleTypeDef *hspi){
-
+HAL_StatusTypeDef spi_cmd_handshake(void){
+    static const uint32_t ack = SPI_ACK_WORD;
+    return HAL_SPI_TransmitReceive_DMA(spi_p, (uint8_t*)&ack, (uint8_t*)&instr.raw, sizeof(instr.raw));
 }
 
 
+/// @brief  Device has sent all requested data, wait for next command.
+/// @param hspi 
+/// @attention Calling `spi_tx_internal` or `spi_tx_external` will lead here.
 void HAL_SPI_TxCpltCallback(SPI_HandleTypeDef *hspi){
-    
+    spi_cmd_handshake();
 }
 
 
-void HAL_SPI_RxHalfCpltCallback(SPI_HandleTypeDef *hspi){
-    
-}
-
-
+/// @brief Device has received all sent data, wait for next command.
+/// @param hspi 
+/// @attention Calling `spi_rx_internal` or `spi_rx_external` will lead here.
 void HAL_SPI_RxCpltCallback(SPI_HandleTypeDef *hspi){
-    uint8_t val = rx_buf[0]++;
-    val++;
-    spi_tx_external(&val, SPI_TX_BUF_SIZE);
+    spi_cmd_handshake();
 }
 
-
-void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin){
-    if(GPIO_Pin == GPIO_PIN_0){
-        spi_rx_internal();
+/// @brief Device has received command and sent its identifier as handshake.
+/// @param hspi 
+/// @attention Calling `spi_cmd_handshake()` will lead here.
+void HAL_SPI_TxRxCpltCallback(SPI_HandleTypeDef *hspi){
+    if(instr.cmd == SPI_REQUEST_DUMMY_DATA){
+        memset32(tx_buf, SPI_DUMMY_WORD, instr.len);
     }
+    spi_tx_internal(instr.len);
 }
--- lib/spi/spi.h
+++ lib/spi/spi.h
@@ -4,13 +4,19 @@
  * @date 2024-02-24
  * @brief SPI peripheral abstraction for MMTool-DSP.
  *
- * The SPI peripheral is configured with a soft-CS on
- * pin PB0 masked as externel interrupt (EXTI0). This
- * interrupt then triggers the flag for the DMA read
- * operation. In addition to the declarations listed 
- * below, the file `spi.c` contains the non-`__weak`
- * definitions of the `HAL_SPI_XxCpltCallback` functions
- * which contain application specific operations.
+ * After lots of trial and error, the SPI abstraction here
+ * runs completely without external or timer interrupts
+ * and without a busy-poll in a loop. DMA reception and
+ * sending is kicked off by the command and data mechanism
+ * present here. SPI starts with a pending TXRX flag for
+ * DMA which is enacted when a command word (32 bit) arrives.
+ * While it arrives, a handshake word is sent back for
+ * identification, it can be found as `SPI_ACK_WORD`.
+ * The process then parses the command and waits for either
+ * a data transmission or reception, which is enacted by
+ * empty frames sent by the host. The expected length of 
+ * the incoming/outgoing data are in the 16 MSBs of the 
+ * command word, the 16 LSBs are the command type.
  */
 
 #ifndef _SPI_H_
@@ -18,15 +24,19 @@
 
 #include "port.h"
 
-#define SPI_TX_BUF_SIZE 1
-#define SPI_RX_BUF_SIZE 1
+#define SPI_TX_BUF_SIZE 2048
+#define SPI_RX_BUF_SIZE 2048
 
 /// @brief Initialize the SPI peripheral abstraction.
 void spi_init(void);
 
+/// @brief De-initialize the SPI peripheral abstraction.
+void spi_deinit(void);
+
 /// @brief Transmit SPI data in DMA-mode from the internal buffer.
+/// @param size Size of buffer data in bytes.
 /// @return `HAL_StatusTypeDef`: Status flag.
-HAL_StatusTypeDef spi_tx_internal(void);
+HAL_StatusTypeDef spi_tx_internal(uint16_t size);
 
 /// @brief Transmit SPI data in DMA-mode from a given buffer. 
 /// @param pData Buffer to transmit.
@@ -35,13 +45,18 @@ HAL_StatusTypeDef spi_tx_internal(void);
 HAL_StatusTypeDef spi_tx_external(uint8_t *pData, uint16_t size);
 
 /// @brief Receive SPI data in DMA-mode into the internal buffer. 
+/// @param size Size of buffer data in bytes.
 /// @return `HAL_StatusTypeDef`: Status flag.
-HAL_StatusTypeDef spi_rx_internal(void);
+HAL_StatusTypeDef spi_rx_internal(uint16_t size);
 
 /// @brief Receive SPI data in DMA-mode into a given buffer.
 /// @param pData Buffer to receive into.
 /// @param size Expected size of buffer data in bytes.
 /// @return `HAL_StatusTypeDef`: Status flag.
 HAL_StatusTypeDef spi_rx_external(uint8_t *pData, uint16_t size);
+
+/// @brief Async-await a 32-bit command and send back a handshake word.
+/// @return `HAL_StatusTypeDef`: Status flag.
+HAL_StatusTypeDef spi_cmd_handshake(void);
 
 #endif // _SPI_H_
